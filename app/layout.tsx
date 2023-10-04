3ca59e1e30534d8a380ef1d233c3f3c74d265bbe
Jake <jake@toomay.dev>
2023-10-03 22:07:27 -0500

    remove header from sign-in page

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import "./globals.css"
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import { ClerkProvider } from "@clerk/nextjs"
-import Header from "@/components/layout/Header"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -23,10 +22,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       }}
     >
       <html lang="en">
-        <body className={inter.className}>
-          <Header />
-          {children}
-        </body>
+        <body className={inter.className}>{children}</body>
       </html>
     </ClerkProvider>
   )
