14993e4cc04f576e8c1e5b3effa5f4085a46069c
Jake <jake@toomay.dev>
2023-10-05 22:47:33 -0500

    Clean Up Header

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -1,31 +1,56 @@
-import { getUser } from "@/app/lib/actions/user.actions"
-import { ClerkProvider, UserButton, useAuth } from "@clerk/nextjs"
+"use client"
+
+import { UserButton } from "@clerk/nextjs"
 import React from "react"
 
 const navigationLinks = [
-  { name: "Home Page", href: "/" },
+  { name: "Home", href: "/" },
   { name: "Profile", href: "/profile" },
 ]
 
-export default async function Header() {
-  const user = await getUser()
-
+export default function Header() {
   return (
     <div className={HeaderStyles}>
-      <nav className={NavigationStyles}>
-        {navigationLinks.map((item) => (
-          <a className={LinkStyles} href={item.href} key={item.href}>
-            {item.name}
-          </a>
-        ))}
-      </nav>
-      <div className={ProfileCardStyles}>
-        <UserButton showName={true} />
-      </div>
+      <Navigation />
+      <ProfileCircle />
     </div>
   )
 }
-const HeaderStyles = "p-5 min-h-[96px] w-full bg-slate-900 flex justify-between"
+
+interface NavItemProps {
+  link: {
+    href: string
+    name: string
+  }
+}
+
+function Navigation() {
+  return (
+    <nav className={NavigationStyles}>
+      {navigationLinks.map((link) => (
+        <NavItem link={link} key={link.href} />
+      ))}
+    </nav>
+  )
+}
+
+function NavItem({ link }: NavItemProps) {
+  return (
+    <a className={LinkStyles} href={link.href} key={link.href}>
+      {link.name}
+    </a>
+  )
+}
+
+function ProfileCircle() {
+  return (
+    <div className={ProfileCardStyles}>
+      <UserButton showName={true} />
+    </div>
+  )
+}
+
+const LinkStyles = "transition-all text-2xl duration-300 hover:text-blue-500 hover:translate-y-[-1px]"
+const HeaderStyles = "py-5 px-10 min-h-[96px] w-full bg-slate-900 flex justify-between"
 const ProfileCardStyles = ""
-const NavigationStyles = " flex gap-4 h-fit "
-const LinkStyles = "transition-colors hover:text-blue-500"
+const NavigationStyles = " flex gap-8 h-fit "
