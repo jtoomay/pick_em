2669efe9e9213424d7c04370e6164816832d9c95
Jake <jake@toomay.dev>
2023-10-09 22:13:11 -0500

    Add back the pagination

--- app/(root)/(home)/Feed.tsx
+++ app/(root)/(home)/Feed.tsx
@@ -10,7 +10,7 @@ export default function Feed({ posts, hasMorePosts }: any) {
           return <Post post={post} key={post.id} />
         })}
       </div>
-      {/* <Paginator hasMorePosts={hasMorePosts} /> */}
+      <Paginator hasMorePosts={hasMorePosts} />
     </div>
   )
 }
